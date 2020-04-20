bdfd8078c7e2e7fbd1cd04d643df06adf90fd95f
SJJ <1621850119@qq.com>
2020-04-20 16:40:44 +0800

    定义子弹类，设定属性和方法，new出一颗新的子弹，在屏幕上画出来

--- src/com/mashibing/tank/TankFrame.java
+++ src/com/mashibing/tank/TankFrame.java
@@ -10,6 +10,7 @@ import java.awt.event.WindowEvent;
 public class TankFrame extends Frame{
 	
 	Tank myTank = new Tank(200, 200, Dir.DOWN);
+	Bullet b = new Bullet(300, 300, Dir.DOWN);
 	
 	public TankFrame() {
 		
@@ -30,6 +31,7 @@ public class TankFrame extends Frame{
 	@Override
 	public void paint(Graphics g) {
 		myTank.paint(g);
+		b.paint(g);
 //		x += 10; 
 //		y += 10; 
 	}
