e0399e56be9ec9e720095d8b98fe0c6d1c5d2f01
SJJ <1621850119@qq.com>
2020-04-20 16:21:04 +0800

    添加坦克静止代码，只要四个方向键都没按，坦克就不动。

--- src/com/mashibing/tank/Tank.java
+++ src/com/mashibing/tank/Tank.java
@@ -6,6 +6,9 @@ public class Tank {
 	private int x, y;
 	private Dir dir = Dir.DOWN;
 	private static final int SPEED = 10;
+	
+	private boolean moving = false;
+	
 	public Tank() {
 		
 	}
@@ -46,6 +49,17 @@ public class Tank {
 	
 	public void paint(Graphics g) {
 		g.fillRect(x, y, 50, 50);
+		move();
+	}
+	
+	public boolean isMoving() {
+		return moving;
+	}
+	public void setMoving(boolean moving) {
+		this.moving = moving;
+	}
+	private void move() {
+		if(!moving) return;
 		switch (dir) {
 		case LEFT:
 			x-=SPEED;
